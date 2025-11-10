ea3e1fd702dd437931b03d9e87a22ca9e3bec13e
Dpixy0 <dpicxy0@gmail.com>
2025-11-10 15:01:48 +0900

    week11 slice v0.5

--- week11/cmd/ch06-ex01/main.go
+++ week11/cmd/ch06-ex01/main.go
@@ -6,6 +6,7 @@ func main() {
 	subjects := [4]string{"Go", "Javascript", "Python", "Linux"} // 슬라이스 리터럴
 	subjectsSlice := subjects[:3]
 	subjects[0] = "Java"
+	subjectsSlice = append(subjectsSlice, "Go", "Database")
 	// subjectsSlice[0] = "Java" // 슬라이싱
 	for _, subject := range subjects {
 		fmt.Println(subject)
