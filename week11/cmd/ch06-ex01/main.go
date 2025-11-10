dfb8207ff5fe8798accffd4606bcfb18aed24624
Dpixy0 <dpicxy0@gmail.com>
2025-11-10 14:56:08 +0900

    week11 slice v0.4

--- week11/cmd/ch06-ex01/main.go
+++ week11/cmd/ch06-ex01/main.go
@@ -4,7 +4,9 @@ import "fmt"
 
 func main() {
 	subjects := [4]string{"Go", "Javascript", "Python", "Linux"} // 슬라이스 리터럴
-	subjectsSlice := subjects[1:3]                               // 슬라이싱
+	subjectsSlice := subjects[:3]
+	subjects[0] = "Java"
+	// subjectsSlice[0] = "Java" // 슬라이싱
 	for _, subject := range subjects {
 		fmt.Println(subject)
 	}
